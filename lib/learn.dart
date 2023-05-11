8723d34d6aa6c40ae59b26b60464cfce877466fd
MusanjeLawrence <musanjelawrence6@gmail.com>
2023-05-11 16:34:29 +0300

    modified learn screen

--- lib/learn.dart
+++ lib/learn.dart
@@ -5,6 +5,7 @@ import 'package:sign_guide/sign_to_text.dart';
 import 'package:sign_guide/text_to_sign.dart';
 import 'package:sign_guide/treasure.dart';
 import 'package:sign_guide/voice_to_sign.dart';
+import 'alphabets.dart';
 import 'dashboard.dart';
 import 'drawer.dart';
 import 'package:sign_guide/main.dart';
@@ -161,73 +162,195 @@ class _LearnState extends State<Learn> {
            ],
          ),
        ),
+      //creating flutter cards to contain links to learn content
       body: Container(child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: GridView(children: [
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Icon(Icons.sort_by_alpha_rounded, size: 50, color: Colors.white,),
-              Text("Alphabets", style: TextStyle(color: Colors.white, fontSize: 22),)
-            ],),
-          ), Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Icon(Icons.numbers_rounded, size: 50, color: Colors.white,),
-              Text("Numbers", style: TextStyle(color: Colors.white, fontSize: 22),)
-            ],),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Alphabets()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.sort_by_alpha_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Alphabets",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ), GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.numbers_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Numbers",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ), GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.color_lens_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Colors",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.color_lens_rounded, size: 50, color: Colors.white,),
-                Text("Colors", style: TextStyle(color: Colors.white, fontSize: 22),)
-              ],),
-          ),
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.handshake, size: 50, color: Colors.white,),
-                Text("Greetings", style: TextStyle(color: Colors.white, fontSize: 22),)
-              ],),
-          ),
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.question_mark_rounded, size: 50, color: Colors.white,),
-                Text("Questions", style: TextStyle(color: Colors.white, fontSize: 22),)
-              ],),
-          ),
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.family_restroom_rounded, size: 50, color: Colors.white,),
-                Text("Family", style: TextStyle(color: Colors.white, fontSize: 22),)
-              ],),
-          ),
-
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.calendar_month_rounded, size: 50, color: Colors.white,),
-                Text("Calendar", style: TextStyle(color: Colors.white, fontSize: 22),)
-              ],),
-          ),
-
-          Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightBlueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.sunny, size: 50, color: Colors.white,),
-                Text("Weather", style: TextStyle(color: Colors.white, fontSize: 22),)
-              ],),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.handshake_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Greetings",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ), GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.question_mark_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Questions",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ), GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.family_restroom_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Family",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ), GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.calendar_month_rounded, size: 50, color: Colors.white),
+                  Text(
+                    "Calendar",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+          ), GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Text_to_Sign()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.sunny, size: 50, color: Colors.white),
+                  Text(
+                    "Weather",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
